043bc38634eaeed55b5891ac689ca313eb7d7bcf
Pietro Paolini <pedr0@pedr0crunchbang>
2015-11-16 20:45:59 +0000

    Columns, boxes and find singleton added.

--- functional-programming-coursework.rkt
+++ functional-programming-coursework.rkt
@@ -15,17 +15,6 @@
 ; This row represents all the possibilities, that is, all the numbers from 1 to 9
 (define TBD `(1 2 3 4 5 6 7 8 9))
 
-;; Transform a single number
-(define (transformNumber x)
-  (if (> x 0)
-      x
-      (apply list TBD)))
-
-;; Transform an entire row
-(define (transformRaw x)
-  (if (empty? x)
-      null
-      (cons (transformNumber (car x)) (transformRaw (cdr x)))))
 
 ;; Transform an entire table table
 (define (transformTable table)
@@ -74,22 +63,68 @@
 
   ;; Return the reverse of it
   (reverse (compute-columns-r list length)))
-  
-      
 
-;; Test extrac()
-;(extract `(0 2 5 0 0 1 0 0 0) 1 1)
 
+
+(define (compute-boxes entry acc)
+
+  (define (compute-offset index)
+    (if (> index 8)
+        -1
+        (* (modulo index 3) 3)))
+
+  (define (compute-raw index)
+    (if (> index 8)
+        -1
+        (* (truncate (/ index 3)) 3)))
+
+  ;; Return a pointer to the first line
+  ;; which is included in the index-th box
+  (define (compute-line entry index)
+    (if (empty? entry)
+        null
+        (drop entry (compute-raw index))))
+
+  ;; Return a pointer to the first element
+  ;; of that line contained in the index-th
+  ;; box
+  ;; The entry parameter points to the line
+  ;; number (index / 3) * 3
+  (define (compute-line-offset entry index)
+    (drop entry (compute-offset index)))
+
+  ;; Create a single box given the pointer
+  (define (compute-box entry index)
+    (define (compute-box-pvt entry index)
+      (list (take (compute-line-offset (car entry) index) 3)
+            (take (compute-line-offset (car (cdr entry)) index) 3)
+            (take (compute-line-offset (car (cdr (cdr entry))) index) 3)))
+    (flatten (compute-box-pvt entry index)))
+
+  (if (> acc 8)
+      null
+      (cons (compute-box (compute-line entry acc) acc) (compute-boxes entry (+ acc 1)))))
+
+;; Test boxes
+(compute-line sampletable 0)
+;(compute-line sampletable 1)
+;(compute-line sampletable 2)
+(compute-line sampletable 3)
+;(compute-line sampletable 4)
+;(compute-line sampletable 5)
+(compute-line sampletable 6)
+;(compute-line sampletable 7)
+;(compute-line sampletable 8)
+
+;(compute-box (compute-line sampletable 0) 0)
+;(compute-box (compute-line sampletable 0) 0)
+;(compute-box (compute-line sampletable 0) 0)
+;(compute-box (compute-line sampletable 0) 0)
+
+(compute-boxes sampletable 0)
 ;; Test nth-column, first column
-;(nth-column sampletable 1)
-;(reverse (compute-columns sampletable 9))
-(compute-columns sampletable 9)
+;(compute-columns sampletable 9)
 
-
-;; Test lines -> column
-;(compute-column sampletable)
-
-
-;(transformRaw `(0 2 5 0 0 1 0 0 0))
+;; Test transformation
 ;(transformTable sampletable)
 
